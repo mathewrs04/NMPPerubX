a5ece0c314788d4d5e19c3b5655fca5d770e2559
HaloRek <150877487+RyanSubagia@users.noreply.github.com>
2024-10-12 18:49:03 +0700

    Upload bentar grgr bluscreen

---
+++ app/src/main/java/com/perubdev/nmpinformaticse_sport/TeamAdapter.kt
@@ -0,0 +1,37 @@
+package com.perubdev.nmpinformaticse_sport
+
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.perubdev.nmpinformaticse_sport.databinding.TeamCardBinding
+
+class TeamAdapter(): RecyclerView.Adapter<TeamAdapter.TeamViewHolder>() {
+    class TeamViewHolder(val binding:TeamCardBinding):RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TeamViewHolder {
+        val binding = TeamCardBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,false)
+        return TeamViewHolder(binding)
+
+    }
+
+    override fun getItemCount(): Int {
+        return GameData.games.size
+    }
+
+
+    override fun onBindViewHolder(holder: TeamViewHolder, position: Int) {
+        holder.binding.imgPurp.setImageResource(R.drawable.purple)
+        holder.binding.txtTeamName.text = GameData.games[position].teams.toString()
+
+        holder.binding.cardSchedule.setOnClickListener {
+            val intent = Intent(holder.itemView.context, SchedulePageDetail::class.java)
+            intent.putExtra(R.string.schedule_index.toString(), position)
+            holder.itemView.context.startActivity(intent)
+        }
+    }
+
+
+}
