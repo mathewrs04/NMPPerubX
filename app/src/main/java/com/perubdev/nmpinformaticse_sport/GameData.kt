8c5c1c4e9c58c6787b7baeaa1e772426667a0d3a
Li sam cong <150877487+RyanSubagia@users.noreply.github.com>
2024-10-16 17:17:45 +0700

    Update Game Data

--- app/src/main/java/com/perubdev/nmpinformaticse_sport/GameData.kt
+++ app/src/main/java/com/perubdev/nmpinformaticse_sport/GameData.kt
@@ -14,21 +14,21 @@ object GameData {
             ),
             arrayOf(
                 TeamBank("Team A", arrayOf(
-                    PlayerBank("Player1", "Duelist", R.drawable.vct),
-                    PlayerBank("Player2", "Controller", R.drawable.vct),
-                    PlayerBank("Player3", "Sentinel", R.drawable.vct),
-                    PlayerBank("Player4", "Initiator", R.drawable.vct),
-                    PlayerBank("Player5", "Flex", R.drawable.vct)
+                    PlayerBank("ANGE1", "Duelist", R.drawable.player),
+                    PlayerBank("SHAO", "Controller", R.drawable.player),
+                    PlayerBank("ERIK", "Sentinel", R.drawable.player),
+                    PlayerBank("LATEKX", "Initiator", R.drawable.player),
+                    PlayerBank("SUYGETSU", "Flex", R.drawable.player)
                 )),
                 TeamBank("Team B", arrayOf(
-                    PlayerBank("Player6", "Duelist", R.drawable.vct),
-                    PlayerBank("Player7", "Controller", R.drawable.vct),
-                    PlayerBank("Player8", "Sentinel", R.drawable.vct),
-                    PlayerBank("Player9", "Initiator", R.drawable.vct),
-                    PlayerBank("Player10", "Flex", R.drawable.vct)
+                    PlayerBank("ZYPPAN", "Duelist", R.drawable.player),
+                    PlayerBank("PARIGIE", "Controller", R.drawable.player),
+                    PlayerBank("WESTON", "Sentinel", R.drawable.player),
+                    PlayerBank("SUITEZ", "Initiator", R.drawable.player),
+                    PlayerBank("TENZ", "Flex", R.drawable.player)
                 ))
             ),
-            R.drawable.valorant
+            R.drawable.vct
         ),
         GameBank(
             "PUBG",
@@ -41,19 +41,48 @@ object GameData {
             ),
             arrayOf(
                 TeamBank("Team B", arrayOf(
-                    PlayerBank("PlayerA", "Sniper", R.drawable.vct),
-                    PlayerBank("PlayerB", "Assault", R.drawable.vct),
-                    PlayerBank("PlayerC", "Support", R.drawable.vct),
-                    PlayerBank("PlayerD", "Medic", R.drawable.vct)
+                    PlayerBank("AXZCER", "Sniper", R.drawable.player),
+                    PlayerBank("PSIX", "Assault", R.drawable.player),
+                    PlayerBank("PUNCHER", "Support", R.drawable.player),
+                    PlayerBank("LEMON", "Medic", R.drawable.player)
                 )),
                 TeamBank("Team C", arrayOf(
-                    PlayerBank("PlayerE", "Sniper", R.drawable.vct),
-                    PlayerBank("PlayerF", "Assault", R.drawable.vct),
-                    PlayerBank("PlayerG", "Support", R.drawable.vct),
-                    PlayerBank("PlayerH", "Medic", R.drawable.vct)
+                    PlayerBank("FLIP", "Sniper", R.drawable.player),
+                    PlayerBank("CHEPALENKO", "Assault", R.drawable.player),
+                    PlayerBank("YELMENBET", "Support", R.drawable.player),
+                    PlayerBank("OMAROV", "Medic", R.drawable.player)
                 ))
             ),
             R.drawable.pgc
+        ),
+        GameBank(
+            "Dota 2",
+            "Multiplayer online battle arena (MOBA)",
+            arrayOf(
+                AchievementsBank("PUBG", "Champion in COMPEDIUM", 2024, "Team A"),
+                AchievementsBank("PUBG", "The International", 2021, "Team B"),
+                AchievementsBank("PUBG", "DreamLeague", 2022 ,"Team A"),
+                AchievementsBank("PUBG", "CCT Series 3", 2023, "Team B")
+            ),
+            arrayOf(
+                TeamBank("Team B", arrayOf(
+                    PlayerBank("CEB", "POS 1", R.drawable.player),
+                    PlayerBank("WISPER", "POS 2", R.drawable.player),
+                    PlayerBank("ARI", "POS 3", R.drawable.player),
+                    PlayerBank("BZM", "POS 4", R.drawable.player),
+                    PlayerBank("KOZAK", "POS 5", R.drawable.player)
+
+                )),
+                TeamBank("Team C", arrayOf(
+                    PlayerBank("PPSAREL", "POS 1", R.drawable.player),
+                    PlayerBank("IZPAH", "POS 2", R.drawable.player),
+                    PlayerBank("SOCKSHKA", "POS 3", R.drawable.player),
+                    PlayerBank("CHU", "POS 4", R.drawable.player),
+                    PlayerBank("LUNA", "POS 5", R.drawable.player)
+                ))
+            ),
+            R.drawable.dota
         )
+
     )
 }
